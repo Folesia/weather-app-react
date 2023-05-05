a502609d9a79b7aaf08c87ac3095a66d37046012
Folesia <folesia@hotmail.com>
2023-05-05 13:18:11 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Weather from"./Weather";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Weather city="Paris"/>
+    </div>
+  );
+}
+
+export default App;
